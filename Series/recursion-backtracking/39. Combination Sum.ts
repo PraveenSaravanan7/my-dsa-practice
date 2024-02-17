e4c6da39615e477aae33b7d01f6e0bc537051a7f
Praveen Saravanan <praveensaravananoff@gmail.com>
2024-02-17 21:16:31 +0530

    combinationSum added

--- Series/recursion-backtracking/39. Combination Sum.ts
+++ Series/recursion-backtracking/39. Combination Sum.ts
@@ -17,3 +17,28 @@ function combinationSum(candidates: number[], target: number): number[][] {
 
   return res;
 }
+
+// /**
+//  * @param {number[]} candidates
+//  * @param {number} target
+//  * @return {number[][]}
+//  */
+// var combinationSum_2 = function (c, target) {
+//   const res = [];
+
+//   const dfs = (i, t, s) => {
+//     if (t < 0) return;
+
+//     if (t === 0) res.push(s.slice());
+
+//     for (let j = i; j < c.length; j++) {
+//       s.push(c[j]);
+//       dfs(j, t - c[j], s);
+//       s.pop();
+//     }
+//   };
+
+//   dfs(0, target, []);
+
+//   return res;
+// };
